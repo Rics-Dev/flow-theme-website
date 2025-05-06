bfabe4b5775d0785c93ce6f872728d11a33b4575
Ric's Dev <ricdev.io@gmail.com>
2025-05-06 18:58:14 +0100

    adjusted for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rics-dev.github.io',
-  base: 'flow-theme-website',
+  base: '/flow-theme-website',
 })
